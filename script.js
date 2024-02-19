b719190fff990c0d0e62f94be0ea84a93f99a862
Murad Kerimov <44784608+Muker2@users.noreply.github.com>
2024-02-19 17:11:38 +0100

    Created Player Object

--- script.js
+++ script.js
@@ -6,5 +6,30 @@ canvas.height = window.innerHeight;
 
 
 context.fillStyle = "black";
-context.fillRect(0,0, canvas.width,canvas.height);
-console.log(context);
+context.fillRect(0,0, canvas.width,     canvas.height);
+
+class Player{
+    constructor({position, velocity}){
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    draw(){
+        context.moveTo(this.position.x + 30, this.position.y);
+        context.lineTo(this.position.x -10, this.position.y - 10);
+        context.lineTo(this.position.x -10, this.position.y + 10);
+        context.closePath();
+
+        context.strokeStyle = "red";
+        context.stroke();
+    }
+}
+
+const player = new Player({
+    position: {x:canvas.width/2, y: canvas.height/2},
+    velocity: {x:0, y: 0}
+})
+
+player.draw();
+
+console.log(player);
