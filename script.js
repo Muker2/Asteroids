7daacc28cc2adf8d73f5da8d43495ead668df107
Murad Kerimov <44784608+Muker2@users.noreply.github.com>
2024-02-25 12:04:54 +0100

    Added moving asteroids

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
+
+const SPEED = 3;
+const ROTATION_SPEED = 0.07;
+const projectiles = [];
+const asteroids = [];
+const PROJECTILE_SPEED = 7;
+
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+window.setInterval(() =>{
+    const index = Math.floor(Math.random() * 4);
+    let x, y;
+    let vx, vy;
+    let radius = 50 * Math.random() + 10;
 
+    switch (index) {
+        //left side
+        case 0: 
+            x = 0 -  radius;
+            y = Math.random() * canvas.height;
+            vx = 1;
+            vy = 0;
+            break;
+        // bottom side
+        case 1: 
+            x = Math.random() * canvas.width;
+            y = canvas.height + radius;
+            vx = 0;
+            vy = -1;
+            break;
+        // right side
+        case 2: 
+            x = canvas.width + radius;
+            y = Math.random() * canvas.height;
+            vx = -1;
+            vy = 0;
+            break;
+        // top side
+        case 3: 
+            x = Math.random() * canvas.width;
+            y = 0 - radius;
+            vx = 0;
+            vy = 1;
+            break;
+    }
 
+    asteroids.push(
+        new Asteroid({
+            position: {
+                x: x,
+                y: y
+            },
+            velocity: {
+                x: vx,
+                y: vy
+            },
+            radius
+        }));
+}, 3000);
 
-class Player{
-    constructor({position, velocity}){
+class Player {
+    constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
         this.rotation = 0;
     }
 
-    draw(){
+    draw() {
 
         context.save();
         context.translate(this.position.x, this.position.y);
@@ -22,14 +77,16 @@ class Player{
         context.rotate(this.rotation)
         context.translate(-this.position.x, -this.position.y);
 
+        context.beginPath();
         context.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
         context.fillStyle = "red";
         context.fill();
+        context.closePath();
 
         context.beginPath();
         context.moveTo(this.position.x + 30, this.position.y);
-        context.lineTo(this.position.x -10, this.position.y - 10);
-        context.lineTo(this.position.x -10, this.position.y + 10);
+        context.lineTo(this.position.x - 10, this.position.y - 10);
+        context.lineTo(this.position.x - 10, this.position.y + 10);
         context.closePath();
 
         context.strokeStyle = "red";
@@ -37,95 +94,129 @@ class Player{
         context.restore();
     }
 
-    update(){
+    update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-}
+    }
 }
 
 
-class Projectile{
-    constructor({position, velocity}){
+class Projectile {
+    constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
         this.radius = 5;
     }
 
-    draw(){
+    draw() {
         context.beginPath();
         context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
         context.closePath();
-        context.fillStyle ="white";
+        context.fillStyle = "white";
         context.fill();
     }
-    
-    update(){
+
+    update() {
         this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
     }
 }
 
+class Asteroid {
+    constructor({ position, velocity, radius }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+    }
 
+    draw() {
+        context.beginPath();
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        context.closePath();
+        context.strokeStyle = "white";
+        context.stroke();
+    }
 
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+}
 
 const player = new Player({
-    position: {x:canvas.width/2, y: canvas.height/2},
-    velocity: {x:0, y: 0}
+    position: { x: canvas.width / 2, y: canvas.height / 2 },
+    velocity: { x: 0, y: 0 }
 })
 
 
 const keys = {
     w: {
         pressed: false
-    } ,
+    },
     a: {
         pressed: false
-    } ,
+    },
     d: {
         pressed: false
-    } 
+    }
 
 }
 
-    const SPEED = 3;
-    const ROTATION_SPEED = 0.07;
-    const projectiles = [];
-
-
-function animate(){
+function animate() {
     window.requestAnimationFrame(animate);
     context.fillStyle = "black";
-    context.fillRect(0,0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
     player.update();
 
-    for(let i = projectiles.length -1; i >= 0; i--){
-        const projectile = projectiles[i];
+    for (let i = asteroids.length - 1; i >= 0; i--) {
+        const asteroid = asteroids[i];
+        if (asteroid.position.x + asteroid.radius < 0 ||
+            asteroid.position.x - asteroid.radius > canvas.width ||
+            asteroid.position.y + asteroid.radius < 0 ||
+            asteroid.position.y - asteroid.radius > canvas.height) {
+            asteroids.splice(i, 1);
+            }
+        asteroid.update();
+
+    }
+
+    for (let i = projectiles.length - 1; i >= 0; i--) {
+        const asteroid = projectiles[i];
+        if (projectile.position.x + projectile.radius < 0 ||
+            projectile.position.x - projectile.radius > canvas.width ||
+            projectile.position.y + projectile.radius < 0 ||
+            projectile.position.y - projectile.radius > canvas.height) {
+            projectiles.splice(i, 1);
+        }
+
         projectile.update();
+
     }
 
     player.velocity.x = 0;
     player.velocity.y = 0;
-    if(keys.w.pressed){ 
+    if (keys.w.pressed) {
         player.velocity.x = Math.cos(player.rotation) * SPEED;
         player.velocity.y = Math.sin(player.rotation) * SPEED;
     }
-    if(keys.d.pressed){
+    if (keys.d.pressed) {
         player.rotation += ROTATION_SPEED;
     }
-    else if(keys.a.pressed){
+    else if (keys.a.pressed) {
         player.rotation -= ROTATION_SPEED;
     }
-    }
+
+}
 
 animate();
 
-console.log(player);
 
 window.addEventListener("keydown", (event) => {
-    switch (event.code){
+    switch (event.code) {
         case "KeyW":
             keys.w.pressed = true;
             break;
@@ -135,24 +226,26 @@ window.addEventListener("keydown", (event) => {
         case "KeyD":
             keys.d.pressed = true;
             break;
-        case"Space":
+        case "Space":
             projectiles.push(
                 new Projectile({
-                position: {
-                    x: player.position.x + Math.cos(player.rotation) * 30,
-                    y: player.position.y + Math.sin(player.rotation) * 30
-                },
-                velocity: {
-                    x: Math.cos(player.rotation) * 10,
-                    y: Math.sin(player.rotation) * 10
-                }
-            }));
+                    position: {
+                        x: player.position.x + Math.cos(player.rotation) * 30,
+                        y: player.position.y + Math.sin(player.rotation) * 30
+                    },
+                    velocity: {
+                        x: Math.cos(player.rotation) * PROJECTILE_SPEED,
+                        y: Math.sin(player.rotation) * PROJECTILE_SPEED
+                    }
+                }));
+                console.log(projectiles);
+
     }
 
 })
 
 window.addEventListener("keyup", (event) => {
-    switch (event.code){
+    switch (event.code) {
         case "KeyW":
             keys.w.pressed = false;
             break;
